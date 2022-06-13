28bd042eb22272083d8f37fa5ce9e1ce7b885f26
changwoo <branden.c.w.kang@gmail.com>
2022-06-13 14:27:39 +0000

    update

--- .ipynb_checkpoints/eval-checkpoint.sh
+++ .ipynb_checkpoints/eval-checkpoint.sh
@@ -1,11 +1,21 @@
 python test.py \
 --name=celeba \
 --path=results \
---input=/root/dev/FinalTerm_DGMS_public_data/input/ \
---mask=/root/dev/FinalTerm_DGMS_public_data/masks/ \
---structure=/root/dev/FinalTerm_DGMS_public_data/pred_structure/ \
---output=/root/dev/FinalTerm_DGMS_public_data/result/ \
---model=2
+--input=/root/dev/Modified_StructureFlow/evaluation_dataset/input/ \
+--mask=/root/dev/Modified_StructureFlow/evaluation_dataset/masks/ \
+--structure=/root/dev/Modified_StructureFlow/evaluation_dataset/pred_structure/ \
+--output=/root/dev/results/celeba/result/ \
+--model=1
+
+
+# python test.py \
+# --name=celeba \
+# --path=results \
+# --input=/root/dev/FinalTerm_DGMS_public_data/input/ \
+# --mask=/root/dev/FinalTerm_DGMS_public_data/masks/ \
+# --structure=/root/dev/FinalTerm_DGMS_public_data/pred_structure/ \
+# --output=/root/dev/FinalTerm_DGMS_public_data/result/ \
+# --model=2
 
 
 #python test.py \
